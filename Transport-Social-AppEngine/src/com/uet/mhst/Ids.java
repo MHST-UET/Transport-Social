536afa2068349ce5253863966bbf0f885e1d00d0
duclv <duclv@duclv-K46CA>
2014-07-26 12:05:42 +0700

    Change android client id

--- Transport-Social-AppEngine/src/com/uet/mhst/Ids.java
+++ Transport-Social-AppEngine/src/com/uet/mhst/Ids.java
@@ -4,6 +4,6 @@ public class Ids {
     public static final String WEB_CLIENT_ID =
             "275231022763-13a5ievdg1ucblrg0buttgogudgmppcs.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID =
-            "275231022763-2dalqm4trurvlnc1319lrheumti2ro8a.apps.googleusercontent.com";
+            "275231022763-up7ehk88ggh9j1crs0msa6l49enuo5p9.apps.googleusercontent.com";
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 }
