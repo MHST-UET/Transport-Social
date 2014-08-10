8c3722eac2f8351d8c91df6502360404f763f2de
duclv <duclv@duclv-K46CA>
2014-08-10 12:10:59 +0700

    test

--- Transport-Social-AppEngine/src/com/uet/mhst/ItemEndpoint.java
+++ Transport-Social-AppEngine/src/com/uet/mhst/ItemEndpoint.java
@@ -1,6 +1,17 @@
 package com.uet.mhst;
 
-import com.uet.mhst.PMF;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.inject.Named;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -10,16 +21,6 @@ import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.users.User;
 import com.google.appengine.datanucleus.query.JDOCursorHelper;
-
-import java.util.HashMap;
-import java.util.List;
-
-import javax.annotation.Nullable;
-import javax.inject.Named;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
-import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
 
 @Api(name = "itemendpoint", namespace = @ApiNamespace(ownerDomain = "uet.com", ownerName = "uet.com", packagePath = "mhst"))
 public class ItemEndpoint {
@@ -35,7 +36,7 @@ public class ItemEndpoint {
 	@ApiMethod(name = "listItem")
 	public CollectionResponse<Item> listItem(
 			@Nullable @Named("cursor") String cursorString,
-			@Nullable @Named("limit") Integer limit) {
+			@Nullable @Named("limit") Integer limit, @Nullable @Named("datetime") Date datetime) {
 
 		PersistenceManager mgr = null;
 		Cursor cursor = null;
@@ -43,8 +44,9 @@ public class ItemEndpoint {
 
 		try {
 			mgr = getPersistenceManager();
-			Query query = mgr.newQuery(Item.class);
-			query.setOrdering("datetime desc");
+			Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Query query = mgr.newQuery("select * from "+Item.class.getName()+" where datetime<DATETIME( '"+formatter.format(datetime)+"' ) order by datetime desc limit 10");
+			//query.setOrdering("datetime desc");
 			if (cursorString != null && cursorString != "") {
 				cursor = Cursor.fromWebSafeString(cursorString);
 				HashMap<String, Object> extensionMap = new HashMap<String, Object>();
